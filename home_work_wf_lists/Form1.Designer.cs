86a953038c4170b45c788aa6fdb9b6919fbeb38c
Василь Колотюк <vasakolotuk02@gmail.com>
2020-11-01 14:53:06 +0200

    first commit

---
+++ home_work_wf_lists/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace home_work_wf_lists
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Employee1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(141, 314);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(121, 20);
+            this.numericUpDown2.TabIndex = 41;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(448, 71);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(121, 41);
+            this.button5.TabIndex = 39;
+            this.button5.Text = " select employee  to change\r\n\r\n";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(676, 393);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 35);
+            this.button4.TabIndex = 38;
+            this.button4.Text = "loading xml";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(676, 351);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 36);
+            this.button3.TabIndex = 37;
+            this.button3.Text = "preservation xml";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(448, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 41);
+            this.button2.TabIndex = 36;
+            this.button2.Text = "removal of the employee";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Employee1
+            // 
+            this.Employee1.FormattingEnabled = true;
+            this.Employee1.Location = new System.Drawing.Point(12, 12);
+            this.Employee1.Name = "Employee1";
+            this.Employee1.Size = new System.Drawing.Size(416, 147);
+            this.Employee1.TabIndex = 35;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(141, 340);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 41);
+            this.button1.TabIndex = 34;
+            this.button1.Text = "creation ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "rivne",
+            "lutsk",
+            "Kiev",
+            "Lviv"});
+            this.comboBox3.Location = new System.Drawing.Point(141, 284);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 33;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(88, 292);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 13);
+            this.label6.TabIndex = 32;
+            this.label6.Text = "enter citi";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "dzerelna",
+            "prosepctova",
+            "stavkavska",
+            "mondelusa"});
+            this.comboBox2.Location = new System.Drawing.Point(141, 257);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 31;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "admin",
+            "programer",
+            "designer"});
+            this.comboBox1.Location = new System.Drawing.Point(141, 232);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 30;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(141, 206);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(121, 20);
+            this.numericUpDown1.TabIndex = 29;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(73, 317);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "enter house";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(75, 261);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "enter street";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "enter position";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "enter salary";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(141, 180);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 24;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "enter name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Employee1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox Employee1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ home_work_wf_lists/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace home_work_wf_lists
+{
+    public partial class Form1 : Form
+    {
+        public List<Employee> employees = new List<Employee>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {         
+            if (button1.Text == "change")
+            {             
+                   employees[Employee1.SelectedIndex].Surname = textBox1.Text;
+                employees[Employee1.SelectedIndex].Salary = numericUpDown1.Value;
+                employees[Employee1.SelectedIndex].Position = comboBox1.Text;
+                employees[Employee1.SelectedIndex].Street = comboBox2.Text;
+                employees[Employee1.SelectedIndex].City = comboBox3.Text;
+                employees[Employee1.SelectedIndex].House = numericUpDown2.Value;
+
+                foreach (var item in employees)
+                {
+                    Employee1.Items.Remove(item);
+                }
+
+                foreach (var item in employees)
+                {
+                    Employee1.Items.Add(item);
+                }           
+                textBox1.ResetText();
+                numericUpDown1.Value = 0;
+                comboBox1.ResetText();
+                comboBox2.ResetText();
+                comboBox3.ResetText();
+                numericUpDown2.Value = 0;
+                button1.Text = "creation";
+            }
+            else
+            {             
+                Employee employee = new Employee();
+
+                if (string.IsNullOrWhiteSpace(textBox1.Text))
+                {
+                    MessageBox.Show("Enter valid Surname!");
+                    return;
+                }
+                else
+                {
+                    employee.Surname = textBox1.Text;
+                }
+
+                if (numericUpDown1.Value > 0 && 10000 >= numericUpDown1.Value)
+                {
+                    employee.Salary = numericUpDown1.Value;
+                }
+                else
+                {
+                    MessageBox.Show("Enter valid salary!");
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(comboBox1.Text))
+                {
+                    MessageBox.Show("Enter valid position!");
+                    return;
+                }
+                else
+                {
+                    employee.Position = comboBox1.Text;
+                }
+
+                if (string.IsNullOrWhiteSpace(comboBox2.Text))
+                {
+                    MessageBox.Show("Enter valid Street!");
+                    return;
+                }
+                else
+                {
+                    employee.Street = comboBox2.Text;
+                }
+
+                if (string.IsNullOrWhiteSpace(comboBox3.Text))
+                {
+                    MessageBox.Show("Enter valid city!");
+                    return;
+                }
+                else
+                {
+                    employee.City = comboBox3.Text;
+                }
+
+
+                if (numericUpDown2.Value > 0 && numericUpDown2.Value < 100)
+                {
+                    employee.House = numericUpDown2.Value;
+                }
+                else
+                {
+                    MessageBox.Show("Enter valid House!");
+                    return;
+                }
+
+                Employee1.Items.Add(employee);
+                employees.Add(employee);
+                textBox1.ResetText();
+                numericUpDown1.Value = 0;
+                comboBox1.ResetText();
+                comboBox2.ResetText();
+                comboBox3.ResetText();
+                numericUpDown2.Value = 0;
+                MessageBox.Show("employee added!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            employees.RemoveAt(Employee1.SelectedIndex);
+            Employee1.Items.RemoveAt(Employee1.SelectedIndex);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (Employee1.SelectedIndex != -1)
+            {
+                button1.Text = "change";
+                textBox1.Text = employees[Employee1.SelectedIndex].Surname;
+                numericUpDown1.Value = employees[Employee1.SelectedIndex].Salary;
+                comboBox1.Text = employees[Employee1.SelectedIndex].Position;
+                comboBox2.Text = employees[Employee1.SelectedIndex].Street;
+                comboBox3.Text = employees[Employee1.SelectedIndex].City;
+                numericUpDown2.Value = employees[Employee1.SelectedIndex].House;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (FileStream fout = new FileStream("file1.xml", FileMode.OpenOrCreate))
+            {
+                XmlSerializer formatter = new XmlSerializer(typeof(List<Employee>));
+                formatter.Serialize(fout, employees);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (FileStream fin = new FileStream("file1.xml", FileMode.OpenOrCreate))
+            {
+                XmlSerializer formatter = new XmlSerializer(typeof(List<Employee>));
+                List<Employee> newlistWorkers = (List<Employee>)formatter.Deserialize(fin);
+                employees = newlistWorkers;
+            }
+
+            foreach (var item in employees)
+            {
+                Employee1.Items.Add(item.ToString());
+            }
+
+        }
+    }
+
+
+
+
+    public class Employee
+    {
+        public string Surname { get; set; }
+        public decimal Salary { get; set; }
+        public string Position { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public decimal House { get; set; }
+        public override string ToString()
+        {
+            return $"Surname: {Surname} \n" +
+                $"Selery: {Salary} \n" +
+                $"Position: {Position} \n" +
+                $"City: {City} \n" +
+                $"Street: {Street} \n" +
+                $"House: {House} \n";
+        }
+
+    }
+}
